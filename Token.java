057157e399bd511f7984b8900c2c961a136377bc
Goldis Razvan <razvylg@gmail.com>
2025-03-13 14:43:47 +0200

    add token constructor

--- Token.java
+++ Token.java
@@ -1,8 +1,50 @@
+
+
+enum code 
+{
+   ID("ID"), BREAK("BREAK"), CHAR("CHAR"), DOUBLE("DOUBLE"), ELSE("ELSE"), FOR("FOR"), IF("IF"), INT("INT"), RETURN("RETURN"), STRUCT("STRUCT"), VOID("VOID"), WHILE("WHILE"), CT_INT("CT_INT"), CT_REAL("CT_REAL"), CT_CHAR("CT_CHAR"), CT_STRING("CT_STRING"), COMMA("COMMA"), SEMICOLON("SEMICOLON"), LPAR("LPAR"), RPAR("RPAR"), LBRACKET("LBRACKET"), RBRACKET("RBRACKET"), LACC("LACC"), RACC("RACC"), ADD("ADD"), SUB("SUB"), MUL("MUL"), DIV("DIV"), DOT("DOT"), AND("AND"), OR("OR"), NOT("NOT"), ASSIGN("ASSIGN"), QUAL("QUAL"), NOTEQ("NOTEQ"), LESS("LESS"), LESSEQ("LESSEQ"), GREATER("GREATER"), GREATEREQ("GREATEREQ");
+
+   String codeName;
+
+   code(String codeName)
+   {
+      this.codeName = codeName;
+   }
+
+}
+
 public class Token 
 {
-   enum code {ID, BREAK, CHAR, DOUBLE, ELSE, FOR, IF, INT, RETURN, STRUCT, VOID, WHILE, CT_INT, CT_REAL, CT_CHAR, CT_STRING, COMMA, SEMICOLON, LPAR, RPAR, LBRACKET, RBRACKET, LACC, RACC, ADD, SUB, MUL, DIV, DOT, AND, OR, NOT, ASSIGN, QUAL, NOTEQ, LESS, LESSEQ, GREATER, GREATEREQ}
-   
+   //code code;
+
+   String code;
+
    Object value;
 
    int line;
+
+   Token(int line, String value, String codeName)
+   {
+      this.line = line;
+      this.code = codeName;
+
+      if(codeName.equals("CT_INT"))
+      {
+         this.value = Integer.valueOf(value);
+         //this.code = code.CT_INT;
+      }
+      else if(codeName.equals("CT_REAL"))
+      {
+         this.value = Double.valueOf(value);
+         //this.code = code.CT_REAL;
+      }
+      else
+      {
+         this.value = value;
+      }
+
+      
+      
+
+   }
 }
