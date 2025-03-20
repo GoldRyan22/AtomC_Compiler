be8ff95abed9391368c7bf8cc857ae3f069aac2b
Goldis Razvan <razvylg@gmail.com>
2025-03-20 19:21:02 +0200

    Update App.java

--- App.java
+++ App.java
@@ -1,7 +1,31 @@
+import java.util.*;
+
+
+
+
 public class App 
 {
     public static void main(String[] args) throws Exception 
     {
-        System.out.println("Hello, World!");
+       // System.out.println("Hello, World!");
+
+        //States state = new CT_INTState();
+
+       // String name = state.getClass().getName();
+
+        //int len = name.length();
+
+        //System.out.println(name.subSequence(0, len-5));
+
+        LexAn lexAn = new LexAn();
+
+         
+
+        ArrayList<Token> tokenList = lexAn.LexicalAnalysis("tests/mytest.c");
+
+        for (Token token : tokenList) 
+        {
+            System.out.println(token.toString());
+        }
     }
 }
